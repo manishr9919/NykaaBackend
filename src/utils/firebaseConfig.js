732caa9a127ef963d15e71a8745fd9149769ff5c
manishr9919 <manishr9919@gamil.com>
2025-03-27 21:07:28 +0530

    change

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../config/nykaaclone-a6943-firebase-adminsdk-fbsvc-22c4c624f9.json");
+const serviceAccount = require("../../config/firebaseConfig.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
