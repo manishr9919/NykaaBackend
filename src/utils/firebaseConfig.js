eb3e8f93640fca701965593d3a7e4c366a979246
manishr9919 <manishr9919@gamil.com>
2025-03-28 20:08:12 +0530

    firebse auth_change

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,6 +1,18 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../config/firebaseConfig.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: "nykaaclone-a6943",
+  private_key_id: process.env.PROJECT_KEY,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+  client_x509_cert_url: process.env.CLIENT_URL,
+  universe_domain: process.env.UNIVERS_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
